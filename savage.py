085c2a094640eae72aa2ce5487da96cc658d83a8
Jamie <jamie.gillett@gmail.com>
2025-04-06 15:00:11 +0100

    implemented ellipse

--- savage.py
+++ savage.py
@@ -24,15 +24,6 @@ class Graphic:
         with open(filepath, 'w') as output:
             output.write(svg_string)
 
-class Circle(Element):
-    def __init__(self, cx, cy, r):
-        self.cx = cx
-        self.cy = cy
-        self.r = r
-        
-    def to_svg(self):
-        svg_string = f"""<circle r="{self.r}" cx="{self.cx}" cy="{self.cy}" />\n"""
-        return svg_string
 
 class Rect(Element):
     def __init__(self, width, height, x, y, rx=None, ry=None):
@@ -52,9 +43,45 @@ class Rect(Element):
         svg_string += "/>\n"
         return svg_string
 
+
+class Circle(Element):
+    def __init__(self, cx, cy, r):
+        self.cx = cx
+        self.cy = cy
+        self.r = r
+        
+    def to_svg(self):
+        svg_string = f"""<circle r="{self.r}" cx="{self.cx}" cy="{self.cy}" />\n"""
+        return svg_string
+
+
+class Ellipse(Element):
+    def __init__(self, cx, cy, rx, ry):
+        self.cx = cx
+        self.cy = cy
+        self.rx = rx
+        self.ry = ry
+    
+    def to_svg(self):
+        svg_string = f"""<ellipse cx="{self.cx}" cy="{self.cy}" rx="{self.rx}" ry="{self.ry}" />\n"""
+        return svg_string
+
+
 class Line(Element):
-    pass
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def to_svg(self):
+        pass
+
 
 class Text(Element):
-    pass
+    def __init__(self):
+        pass
+
+    def to_svg(self):
+        pass
 
