2740ab50cac3aed3523147eb2974614ab9ecccf0
Jamie <jamie.gillett@gmail.com>
2025-04-07 12:44:14 +0100

    added Group boiler plate

---
+++ savage/elements/group.py
@@ -0,0 +1,12 @@
+from ..element import Element
+
+class Group(Element):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.children = []
+        
+    def add(self):
+        pass
+        
+    def to_svg(self):
+        return super().to_svg()
