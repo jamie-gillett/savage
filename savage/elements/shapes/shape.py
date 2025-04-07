d423b20799ea4eb2d8fa74b4afe130a0eb174246
Jamie <jamie.gillett@gmail.com>
2025-04-07 23:04:38 +0100

    adding shape(element) base class moving circle to shape

---
+++ savage/elements/shapes/shape.py
@@ -0,0 +1,10 @@
+from ..element import Element
+
+class Shape(Element):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+    def to_svg(self):
+        svg_string = self.generate_open_tag_svg()
+        svg_string = svg_string.replace(">"," />")
+        return svg_string
