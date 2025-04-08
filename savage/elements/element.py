a3556db364bd2138dd6a541b4c92724be92290ce
Jamie <jamie.gillett@gmail.com>
2025-04-08 18:12:09 +0100

    added transformations

--- savage/elements/element.py
+++ savage/elements/element.py
@@ -7,6 +7,32 @@ class Element:
         self.styles['fill'] = kwargs.get('fill', None)
         self.styles['stroke'] = kwargs.get('stroke', None)
         self.styles['stroke-width'] = kwargs.get('strokewidth', None)
+        self.transformations = []
+        
+    def translate(self, dx, dy):
+        self.transformations.append(f"translate({dx},{dy})")
+        return self
+    
+    def rotate(self, angle, cx=None, cy=None):
+        if cx is not None and cy is not None:
+            self.transformations.append(f"rotate({angle} {cx} {cy})")
+        else:
+            self.transformations.append(f"rotate({angle})")
+        return self
+    
+    def scale(self, sx, sy=None):
+        if sy is not None:
+            self.transformations.append(f"scale({sx} {sy})")
+        else:
+            self.transformations.append(f"scale({sx})")
+        return self
+    
+    def skew(self, x=None, y=None):
+        if x is not None:
+            self.transformations.append(f"skewX({x})")
+        if y is not None:
+            self.transformations.append(f"skewY({y})")
+        return self
 
     def to_svg(self):
         svg_string = self.add_open_tag_svg()
@@ -15,7 +41,7 @@ class Element:
         return svg_string
 
     def add_open_tag_svg(self):
-        open_tag_svg = f"<{self.tag}{self.add_attribute_svg()}{self.add_style_svg()}>"
+        open_tag_svg = f"<{self.tag}{self.add_attribute_svg()}{self.add_style_svg()}{self.add_transform_svg()}>"
         return open_tag_svg
 
     def add_attribute_svg(self):
@@ -24,12 +50,17 @@ class Element:
             attribute_svg += f' {attribute}="{value}"'
         return attribute_svg
 
-    def add_content_svg(self):
-        return self.content
-    
     def add_style_svg(self):
         styling_svg = ""
         for style_attribute, value in self.styles.items():
             if value:
                 styling_svg += f' {style_attribute}="{value}"'
         return styling_svg
+
+    def add_transform_svg(self):
+        if self.transformations:
+            return f''' transform="{" ".join(self.transformations)}"'''
+        return ""
+
+    def add_content_svg(self):
+        return self.content
