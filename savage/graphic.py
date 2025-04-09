3c3341606fc0e20701bbbcb5d19fc5bfda609c40
Jamie <jamie.gillett@gmail.com>
2025-04-09 19:26:57 +0100

    allows for no width or height specification in Graphic, allows for bg colourin Graphic, changed indentation from 4 spaces to 2

--- savage/graphic.py
+++ savage/graphic.py
@@ -1,9 +1,10 @@
 from elements import Element
 
 class Graphic:
-    def __init__(self, width, height):
+    def __init__(self, width=None, height=None, background=None):
         self.width = width
         self.height = height
+        self.background = background
         self.elements: list[Element] = []
         
     def add(self, element:Element):
@@ -11,9 +12,13 @@ class Graphic:
 
     def to_svg(self):
         svg_string = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n"""
-        svg_string += f"""<svg xmlns="http://www.w3.org/2000/svg" width="{self.width}" height="{self.height}">\n"""
+        width_svg = f' width="{self.width}"' if self.width is not None else ""
+        height_svg = f' height="{self.height}"' if self.height is not None else ""
+        svg_string += f"""<svg xmlns="http://www.w3.org/2000/svg"{width_svg}{height_svg}>\n"""
+        if self.background:
+            svg_string += f'  <rect width="100%" height="100%" fill="{self.background}" />\n'
         for element in self.elements:
-            svg_string += "    " + element.to_svg() + "\n"
+            svg_string += "  " + element.to_svg() + "\n"
         svg_string += """</svg>"""
         return svg_string
     
